78aec6d0ae2d45c3da75c9d01652f1098bd707a4
Yash Sadhukhan <yskhan61@gmail.com>
2024-03-26 17:31:23 +0530

    Adding listener in Player
    Add Listeners to LightSwitchView and PlayerController to add custom logics

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -8,9 +8,16 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
-    private void OnEnable() => EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
-
-    private void OnDisable() => EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+    private void OnEnable()
+    {
+        EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+        EventService.Instance.OnLightsOffByGhostEvent.AddListener(onLightsOffByGhost);
+    }
+    private void OnDisable()
+    {
+        EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+        EventService.Instance.OnLightsOffByGhostEvent.RemoveListener(onLightsOffByGhost);
+    }
 
     private void Start() => currentState = SwitchState.Off;
 
@@ -44,5 +51,22 @@ public class LightSwitchView : MonoBehaviour, IInteractable
         toggleLights();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
         GameService.Instance.GetInstructionView().HideInstruction();
+    }
+
+    private void onLightsOffByGhost()
+    {
+        TurnOffLights();
+
+        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
+        GameService.Instance.GetInstructionView().HideInstruction();
+    }
+
+    private void TurnOffLights()
+    {
+        currentState = SwitchState.Off;
+        foreach (Light lightSource in lightsources)
+        {
+            lightSource.enabled = false;
+        }
     }
 }
